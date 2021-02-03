355cc91bea1fe2e75de23ae97b37de7a1c590517
vantu <nguyenvantu2972002@gmail.com>
2021-02-03 17:04:55 +0700

    done

---
+++ src/main/java/service/CustomerService.java
@@ -0,0 +1,45 @@
+package service;
+
+import model.Customer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomerService implements ICustomerService{
+    private static Map<Integer, Customer> customers;
+
+    static {
+        customers = new HashMap<>();
+customers.put(1,new Customer(1,"tu1","tutuan1@gmail.com","nghe an"));
+customers.put(2,new Customer(2,"tu2","tutuan2@gmail.com","nghe an"));
+customers.put(3,new Customer(3,"tu3","tutuan3@gmail.com","nghe an"));
+customers.put(4,new Customer(4,"tu4","tutuan4@gmail.com","nghe an"));
+customers.put(5,new Customer(5,"tu5","tutuan5@gmail.com","nghe an"));
+    }
+    @Override
+    public List<Customer> findAll() {
+        return new ArrayList<>(customers.values());
+    }
+
+    @Override
+    public Customer save(Customer customer) {
+        return customers.put(customer.getId(), customer);
+    }
+
+    @Override
+    public Customer updateById(int id, Customer customer) {
+        return customers.replace(id, customer);
+    }
+
+    @Override
+    public Customer findById(int id) {
+        return customers.get(id);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        customers.remove(id);
+    }
+}
